581e808f424cbeee38baea30b4c9d144e264cf10
bsuth <bsuth@umich.edu>
2019-01-15 18:42:59 -0500

    finalize study guide styles and class names

--- src/components/StudyGuide.js
+++ src/components/StudyGuide.js
@@ -8,7 +8,7 @@ import './StudyGuide.scss'
 export default ({book, num, children}) => (
 	<div className="page">
 
-		<h2 className="blah">
+		<h2 className="study_guide_title">
 			{book} Study Guide
 			<br/>
 			Section {num.toString().padStart(3, '0')}
