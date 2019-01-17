7f72262af488487c7b1d2555a11ca39ffc8af917
bsuth <bsuth@umich.edu>
2019-01-17 18:18:58 -0500

    populate kokoro

---
+++ src/kokoro/Chapter2/StudyGuide22.js
@@ -0,0 +1,63 @@
+// React
+import React from 'react'
+
+// Components
+import StudyGuide from '../../components/StudyGuide'
+
+
+export default () => (
+	<StudyGuide book='kokoro' num={22}>
+
+		<h3 class="section">Practice text with full furigana</h3>
+		<p><span><ruby><rb>私</rb><rt>わたくし</rt></ruby></span>が<span><ruby><rb>奥</rb><rt>おく</rt></ruby>さん</span>と<span><ruby><rb>話</rb><rt>はな</rt></ruby>して</span>いる<span><ruby><rb>間</rb><rt>あいだ</rt></ruby>に</span>、<span><ruby><rb>問題</rb><rt>もんだい</rt></ruby></span>が<span><ruby><rb>自然</rb><rt>しぜん</rt></ruby></span><span><ruby><rb>先生</rb><rt>せんせい</rt></ruby></span><span>の<ruby><rb>事</rb><rt>こと</rt></ruby></span>からそこへ<span><ruby><rb>落</rb><rt>お</rt></ruby>ちて<ruby><rb>来</rb><rt>き</rt></ruby>た</span>。</p>
+		<p>「先生はなぜああやって、<span><ruby><rb>宅</rb><rt>うち</rt></ruby></span>で<span><ruby><rb>考</rb><rt>かんが</rt></ruby>えたり</span><span><ruby><rb>勉強</rb><rt>べんきょう</rt></ruby>したり</span>なさるだけで、<span><ruby><rb>世</rb><rt>よ</rt></ruby>の<ruby><rb>中</rb><rt>なか</rt></ruby></span>へ<span><ruby><rb>出</rb><rt>で</rt></ruby>て</span><span><ruby><rb>仕事</rb><rt>しごと</rt></ruby></span>をなさらないんでしょう」</p>
+		<p>「あの<span><ruby><rb>人</rb><rt>ひと</rt></ruby></span>は<span><ruby><rb>駄目</rb><rt>だめ</rt></ruby></span>ですよ。そういう事が<span><ruby><rb>嫌</rb><rt>きら</rt></ruby>い</span>なんですから」</p>
+		<p>「つまり<span><ruby><rb>下</rb><rt>くだ</rt></ruby>らない</span>事だと<span><ruby><rb>悟</rb><rt>さと</rt></ruby>って</span>いらっしゃるんでしょうか」</p>
+		<p>「悟るの悟らないのって、――そりゃ<span><ruby><rb>女</rb><rt>おんな</rt></ruby></span>だからわたくしには<span><ruby><rb>解</rb><rt>わか</rt></ruby>りません</span>けれど、おそらくそんな<span><ruby><rb>意味</rb><rt>いみ</rt></ruby></span>じゃないでしょう。やっぱり<span><ruby><rb>何</rb><rt>なに</rt></ruby>か</span>やりたいのでしょう。それでいてできないんです。だから<span><ruby><rb>気</rb><rt>き</rt></ruby>の<ruby><rb>毒</rb><rt>どく</rt></ruby></span>ですわ」</p>
+		<p>「しかし先生は<span><ruby><rb>健康</rb><rt>けんこう</rt></ruby></span>からいって、<span><ruby><rb>別</rb><rt>べつ</rt></ruby>に</span>どこも<span><ruby><rb>悪</rb><rt>わる</rt></ruby>い</span>ところはないようじゃありませんか」</p>
+		<p>「<span><ruby><rb>丈夫</rb><rt>じょうぶ</rt></ruby></span>ですとも。<span><ruby><rb>何</rb><rt>なん</rt></ruby>にも</span><span><ruby><rb>持病</rb><rt>じびょう</rt></ruby></span>はありません」</p>
+		<p>「それでなぜ<span><ruby><rb>活動</rb><rt>かつどう</rt></ruby></span>ができないんでしょう」</p>
+		<p>「それが解らないのよ、あなた。それが解るくらいなら私だって、こんなに<span><ruby><rb>心配</rb><rt>しんぱい</rt></ruby></span>しやしません。わからないから気の毒でたまらないんです」</p>
+		<p>奥さんの<span><ruby><rb>語気</rb><rt>ごき</rt></ruby></span>には<span><ruby><rb>非常</rb><rt>ひじょう</rt></ruby>に</span><span><ruby><rb>同情</rb><rt>どうじょう</rt></ruby></span>があった。それでも<span><ruby><rb>口元</rb><rt>くちもと</rt></ruby></span>だけには<span><ruby><rb>微笑</rb><rt>びしょう</rt></ruby></span>が<span><ruby><rb>見</rb><rt>み</rt></ruby>えた</span>。<span><ruby><rb>外側</rb><rt>そとがわ</rt></ruby></span>からいえば、私の<span><ruby><rb>方</rb><rt>ほう</rt></ruby></span>がむしろ<span><ruby><rb>真面目</rb><rt>まじめ</rt></ruby></span>だった。私はむずかしい<span><ruby><rb>顔</rb><rt>かお</rt></ruby></span>をして<span><ruby><rb>黙</rb><rt>だま</rt></ruby>っていた</span>。すると奥さんが<span><ruby><rb>急</rb><rt>きゅう</rt></ruby>に</span><span><ruby><rb>思</rb><rt>おも</rt></ruby>い<ruby><rb>出</rb><rt>だ</rt></ruby>した</span>ようにまた<span><ruby><rb>口</rb><rt>くち</rt></ruby>を<ruby><rb>開</rb><rt>ひら</rt></ruby>いた</span>。</p>
+		<p>「<span><ruby><rb>若</rb><rt>わか</rt></ruby>い</span><span><ruby><rb>時</rb><rt>とき</rt></ruby></span>はあんな人じゃなかったんですよ。若い時はまるで<span><ruby><rb>違</rb><rt>ちが</rt></ruby>っていました</span>。それが<span><ruby><rb>全</rb><rt>まった</rt></ruby>く</span><span><ruby><rb>変</rb><rt>かわ</rt></ruby>ってしまった</span>んです」</p>
+		<p>「若い時って<span>いつ<ruby><rb>頃</rb><rt>ごろ</rt></ruby></span>ですか」と私が<span><ruby><rb>聞</rb><rt>き</rt></ruby>いた</span>。</p>
+		<p>「<span><ruby><rb>書生</rb><rt>しょせい</rt></ruby><ruby><rb>時代</rb><rt>じだい</rt></ruby></span>よ」</p>
+		<p>「書生時代から先生を<span><ruby><rb>知</rb><rt>し</rt></ruby>って</span>いらっしゃったんですか」</p>
+		<p>奥さんは急に<span><ruby><rb>薄赤</rb><rt>うすあか</rt></ruby>い</span>顔をした。</p>
+
+		<h3 class="section">Rough translation</h3>
+		<p>As I talked with Sensei's wife, our discussion, in due course, fell first to Sensei and then to this very subject.</p>
+		<p>"Why is it that Sensei is so studious in his own home yet not engaged with the world outside?"</p>
+		<p>"Engagement is out of the question. He won't even think of it."</p>
+		<p>"Because he views it as futile?"</p>
+		<p>"I can't say how he views the world -- as a woman it's not my prerogative to know. However, I don't believe that's the case. I think he wants to engage. He wants to, but he can't. It's terribly unfortunate."</p>
+		<p>"But he seems quite capable. He's perfectly healthy, is he not?"</p>
+		<p>"His health is fine. There's nothing wrong with him."</p>
+		<p>"Then what could stop him from pursuing an occupation?"</p>
+		<p>"I'm afraid I can't answer. If I knew that much, then I wouldn't worry so. I'm in the dark, unable to ease his plight."</p>
+		<p>In her voice was great sympathy. Still, she managed to put on a faint smile. On the surface at least, I was the more intent one, brooding in silence. As if suddenly remembering, she spoke again.</p>
+		<p>"He wasn't always like this. He was a different man in his youth. He's changed completely."</p>
+		<p>"How long ago do you mean?"</p>
+		<p>"His student days."</p>
+		<p>"You knew Sensei in his student days?"</p>
+		<p>She blushed a bit.</p>
+
+		<h3 class="section">Vocabulary</h3>
+		<span class="vocabstudy"><ruby><rb>私</rb><rt>わたくし</rt></ruby>&nbsp;I; me</span><span class="vocabstudy"><ruby><rb>奥</rb><rt>おく</rt></ruby>さん&nbsp;Sensei's wife</span><span class="vocabstudy"><ruby><rb>話</rb><rt>はな</rt></ruby>して&nbsp;talk; converse (with)</span><span class="vocabstudy"><ruby><rb>間</rb><rt>あいだ</rt></ruby>に&nbsp;while ...</span><span class="vocabstudy"><ruby><rb>問題</rb><rt>もんだい</rt></ruby>&nbsp;subject; topic</span><span class="vocabstudy"><ruby><rb>自然</rb><rt>しぜん</rt></ruby>&nbsp;naturally; in due course</span><span class="vocabstudy"><ruby><rb>先生</rb><rt>せんせい</rt></ruby>&nbsp;Sensei (elder one; teacher - used here as form of address)</span><span class="vocabstudy">の<ruby><rb>事</rb><rt>こと</rt></ruby>&nbsp;concerning ...</span><span class="vocabstudy"><ruby><rb>落</rb><rt>お</rt></ruby>ちて<ruby><rb>来</rb><rt>き</rt></ruby>た&nbsp;fell to; landed on</span>
+		<span class="vocabstudy"><ruby><rb>宅</rb><rt>うち</rt></ruby>&nbsp;house; home</span><span class="vocabstudy"><ruby><rb>考</rb><rt>かんが</rt></ruby>えたり&nbsp;consider; contemplate; reflect on</span><span class="vocabstudy"><ruby><rb>勉強</rb><rt>べんきょう</rt></ruby>したり&nbsp;study; research</span><span class="vocabstudy"><ruby><rb>世</rb><rt>よ</rt></ruby>の<ruby><rb>中</rb><rt>なか</rt></ruby>&nbsp;society; the world</span><span class="vocabstudy"><ruby><rb>出</rb><rt>で</rt></ruby>て&nbsp;go into; appear in</span><span class="vocabstudy"><ruby><rb>仕事</rb><rt>しごと</rt></ruby>&nbsp;occupation; employment</span>
+		<span class="vocabstudy"><ruby><rb>人</rb><rt>ひと</rt></ruby>&nbsp;person; man</span><span class="vocabstudy"><ruby><rb>駄目</rb><rt>だめ</rt></ruby>&nbsp;hopeless; impossible; out of the question</span><span class="vocabstudy"><ruby><rb>嫌</rb><rt>きら</rt></ruby>い&nbsp;not to one's liking</span>
+		<span class="vocabstudy"><ruby><rb>下</rb><rt>くだ</rt></ruby>らない&nbsp;foolish; of no worth</span><span class="vocabstudy"><ruby><rb>悟</rb><rt>さと</rt></ruby>って&nbsp;perceive; sense (as)</span>
+		<span class="vocabstudy"><ruby><rb>女</rb><rt>おんな</rt></ruby>&nbsp;woman</span><span class="vocabstudy"><ruby><rb>解</rb><rt>わか</rt></ruby>りません&nbsp;don't know; can't tell</span><span class="vocabstudy"><ruby><rb>意味</rb><rt>いみ</rt></ruby>&nbsp;meaning; sense</span><span class="vocabstudy"><ruby><rb>何</rb><rt>なに</rt></ruby>か&nbsp;something</span><span class="vocabstudy"><ruby><rb>気</rb><rt>き</rt></ruby>の<ruby><rb>毒</rb><rt>どく</rt></ruby>&nbsp;unfortunate; pitiable</span>
+		<span class="vocabstudy"><ruby><rb>健康</rb><rt>けんこう</rt></ruby>&nbsp;health; physical condition</span><span class="vocabstudy"><ruby><rb>別</rb><rt>べつ</rt></ruby>に&nbsp;particularly</span><span class="vocabstudy"><ruby><rb>悪</rb><rt>わる</rt></ruby>い&nbsp;poor; inferior; out of sorts</span>
+		<span class="vocabstudy"><ruby><rb>丈夫</rb><rt>じょうぶ</rt></ruby>&nbsp;healthy; strong; robust</span><span class="vocabstudy"><ruby><rb>何</rb><rt>なん</rt></ruby>にも&nbsp;(no)thing</span><span class="vocabstudy"><ruby><rb>持病</rb><rt>じびょう</rt></ruby>&nbsp;chronic illness</span>
+		<span class="vocabstudy"><ruby><rb>活動</rb><rt>かつどう</rt></ruby>&nbsp;endeavor; effort; engagement</span>
+		<span class="vocabstudy"><ruby><rb>心配</rb><rt>しんぱい</rt></ruby>&nbsp;worry</span>
+		<span class="vocabstudy"><ruby><rb>語気</rb><rt>ごき</rt></ruby>&nbsp;tone of voice; manner of speaking</span><span class="vocabstudy"><ruby><rb>非常</rb><rt>ひじょう</rt></ruby>に&nbsp;very much; exceedingly</span><span class="vocabstudy"><ruby><rb>同情</rb><rt>どうじょう</rt></ruby>&nbsp;sympathy; compassion</span><span class="vocabstudy"><ruby><rb>口元</rb><rt>くちもと</rt></ruby>&nbsp;mouth; lips</span><span class="vocabstudy"><ruby><rb>微笑</rb><rt>びしょう</rt></ruby>&nbsp;smile; hint of a smile</span><span class="vocabstudy"><ruby><rb>見</rb><rt>み</rt></ruby>えた&nbsp;appeared</span><span class="vocabstudy"><ruby><rb>外側</rb><rt>そとがわ</rt></ruby>&nbsp;outside; exterior</span><span class="vocabstudy"><ruby><rb>方</rb><rt>ほう</rt></ruby>&nbsp;alternative (of two things)</span><span class="vocabstudy"><ruby><rb>真面目</rb><rt>まじめ</rt></ruby>&nbsp;serious; intent</span><span class="vocabstudy"><ruby><rb>顔</rb><rt>かお</rt></ruby>&nbsp;face; facial expression</span><span class="vocabstudy"><ruby><rb>黙</rb><rt>だま</rt></ruby>っていた&nbsp;remained silent</span><span class="vocabstudy"><ruby><rb>急</rb><rt>きゅう</rt></ruby>に&nbsp;suddenly</span><span class="vocabstudy"><ruby><rb>思</rb><rt>おも</rt></ruby>い<ruby><rb>出</rb><rt>だ</rt></ruby>した&nbsp;remembered; recollected</span><span class="vocabstudy"><ruby><rb>口</rb><rt>くち</rt></ruby>を<ruby><rb>開</rb><rt>ひら</rt></ruby>いた&nbsp;spoke</span>
+		<span class="vocabstudy"><ruby><rb>若</rb><rt>わか</rt></ruby>い&nbsp;young</span><span class="vocabstudy"><ruby><rb>時</rb><rt>とき</rt></ruby>&nbsp;time; period</span><span class="vocabstudy"><ruby><rb>違</rb><rt>ちが</rt></ruby>っていました&nbsp;was different</span><span class="vocabstudy"><ruby><rb>全</rb><rt>まった</rt></ruby>く&nbsp;completely; utterly</span><span class="vocabstudy"><ruby><rb>変</rb><rt>かわ</rt></ruby>ってしまった&nbsp;changed</span>
+		<span class="vocabstudy">いつ<ruby><rb>頃</rb><rt>ごろ</rt></ruby>&nbsp;(about) when</span><span class="vocabstudy"><ruby><rb>聞</rb><rt>き</rt></ruby>いた&nbsp;asked</span>
+		<span class="vocabstudy"><ruby><rb>書生</rb><rt>しょせい</rt></ruby><ruby><rb>時代</rb><rt>じだい</rt></ruby>&nbsp;student days</span>
+		<span class="vocabstudy"><ruby><rb>知</rb><rt>し</rt></ruby>って&nbsp;know; be acquainted with</span>
+		<span class="vocabstudy"><ruby><rb>薄赤</rb><rt>うすあか</rt></ruby>い&nbsp;lightly flushed (red)</span>
+
+	</StudyGuide>
+);
---
+++ src/kokoro/Chapter4/StudyGuide69.js
@@ -0,0 +1,76 @@
+// React
+import React from 'react'
+
+// Components
+import StudyGuide from '../../components/StudyGuide'
+
+
+export default () => (
+	<StudyGuide book='kokoro' num={69}>
+
+		<h3 class="section">Practice text with full furigana</h3>
+		<p><span><ruby><rb>三十五</rb><rt>さんじゅうご</rt></ruby></span></p>
+		<p><span><ruby><rb>私</rb><rt>わたくし</rt></ruby></span>は<span><ruby><rb>立</rb><rt>た</rt></ruby>て<ruby><rb>掛</rb><rt>か</rt></ruby>けた</span><span><ruby><rb>腰</rb><rt>こし</rt></ruby></span>をまたおろして、<span><ruby><rb>話</rb><rt>はなし</rt></ruby></span>の<span><ruby><rb>区切</rb><rt>くぎ</rt></ruby>りの<ruby><rb>付</rb><rt>つ</rt></ruby>く</span>まで<span><ruby><rb>二人</rb><rt>ふたり</rt></ruby></span>の<span><ruby><rb>相手</rb><rt>あいて</rt></ruby></span>になっていた。</p>
+		<p>「<span><ruby><rb>君</rb><rt>きみ</rt></ruby></span>はどう<span><ruby><rb>思</rb><rt>おも</rt></ruby>います</span>」と<span><ruby><rb>先生</rb><rt>せんせい</rt></ruby></span>が<span><ruby><rb>聞</rb><rt>き</rt></ruby>いた</span>。</p>
+		<p>先生が<span><ruby><rb>先</rb><rt>さき</rt></ruby></span>へ<span><ruby><rb>死</rb><rt>し</rt></ruby>ぬ</span>か、<span><ruby><rb>奥</rb><rt>おく</rt></ruby>さん</span>が<span><ruby><rb>早</rb><rt>はや</rt></ruby>く</span><span><ruby><rb>亡</rb><rt>な</rt></ruby>くなる</span>か、<span><ruby><rb>固</rb><rt>もと</rt></ruby>より</span>私に<span><ruby><rb>判断</rb><rt>はんだん</rt></ruby></span>のつくべき<span><ruby><rb>問題</rb><rt>もんだい</rt></ruby></span>ではなかった。私はただ<span><ruby><rb>笑</rb><rt>わら</rt></ruby>っていた</span>。</p>
+		<p>「<span><ruby><rb>寿命</rb><rt>じゅみょう</rt></ruby></span>は<span><ruby><rb>分</rb><rt>わか</rt></ruby>りません</span>ね。私にも」</p>
+		<p>「こればかりは<span><ruby><rb>本当</rb><rt>ほんとう</rt></ruby>に</span>寿命ですからね。<span><ruby><rb>生</rb><rt>うま</rt></ruby>れた</span><span><ruby><rb>時</rb><rt>とき</rt></ruby></span>にちゃんと<span><ruby><rb>極</rb><rt>きま</rt></ruby>った</span><span><ruby><rb>年数</rb><rt>ねんすう</rt></ruby></span>を<span>もらって<ruby><rb>来</rb><rt>く</rt></ruby>る</span>んだから<span><ruby><rb>仕方</rb><rt>しかた</rt></ruby>がない</span>わ。先生の<span>お<ruby><rb>父</rb><rt>とう</rt></ruby>さん</span>や<span>お<ruby><rb>母</rb><rt>かあ</rt></ruby>さん</span>なんか、ほとんど<span><ruby><rb>同</rb><rt>おんな</rt></ruby>じ</span>よ、あなた、亡くなったのが」</p>
+		<p>「亡くなられた<span><ruby><rb>日</rb><rt>ひ</rt></ruby></span>がですか」</p>
+		<p>「まさか日まで同じじゃないけれども。でもまあ同じよ。だって<span><ruby><rb>続</rb><rt>つづ</rt></ruby>いて</span>亡くなっちまったんですもの」</p>
+		<p>この<span><ruby><rb>知識</rb><rt>ちしき</rt></ruby></span>は私にとって<span><ruby><rb>新</rb><rt>あたら</rt></ruby>しい</span>ものであった。私は<span><ruby><rb>不思議</rb><rt>ふしぎ</rt></ruby></span>に思った。</p>
+		<p>「どうしてそう<span><ruby><rb>一度</rb><rt>いちど</rt></ruby>に</span>死なれたんですか」</p>
+		<p>奥さんは私の<span><ruby><rb>問</rb><rt>と</rt></ruby>い</span>に<span><ruby><rb>答</rb><rt>こた</rt></ruby>えよう</span>とした。先生はそれを<span><ruby><rb>遮</rb><rt>さえぎ</rt></ruby>った</span>。</p>
+		<p>「そんな話は<span>お<ruby><rb>止</rb><rt>よ</rt></ruby>し</span>よ。つまらないから」</p>
+		<p>先生は<span><ruby><rb>手</rb><rt>て</rt></ruby></span>に<span><ruby><rb>持</rb><rt>も</rt></ruby>った</span><span><ruby><rb>団扇</rb><rt>うちわ</rt></ruby></span>をわざとばたばたいわせた。そうしてまた奥さんを<span><ruby><rb>顧</rb><rt>かえり</rt></ruby>みた</span>。</p>
+		<p>「<span><ruby><rb>静</rb><rt>しず</rt></ruby></span>、おれが死んだらこの<span><ruby><rb>家</rb><rt>うち</rt></ruby></span>を<span>お<ruby><rb>前</rb><rt>まえ</rt></ruby></span>にやろう」</p>
+		<p>奥さんは<span><ruby><rb>笑</rb><rt>わら</rt></ruby>い<ruby><rb>出</rb><rt>だ</rt></ruby>した</span>。</p>
+		<p>「ついでに<span><ruby><rb>地面</rb><rt>じめん</rt></ruby></span>も<span><ruby><rb>下</rb><rt>くだ</rt></ruby>さい</span>よ」</p>
+		<p>「地面は<span><ruby><rb>他</rb><rt>ひと</rt></ruby></span>のものだから仕方がない。<span>その<ruby><rb>代</rb><rt>かわ</rt></ruby>り</span>おれの持ってるものは<span><ruby><rb>皆</rb><rt>みん</rt></ruby>な</span>お前にやるよ」</p>
+		<p>「どうも<span><ruby><rb>有難</rb><rt>ありがと</rt></ruby>う</span>。けれども<span><ruby><rb>横文字</rb><rt>よこもじ</rt></ruby></span>の<span><ruby><rb>本</rb><rt>ほん</rt></ruby></span>なんか<span><ruby><rb>貰</rb><rt>もら</rt></ruby>って</span>も仕様がないわね」</p>
+		<p>「<span><ruby><rb>古本屋</rb><rt>ふるほんや</rt></ruby></span>に<span><ruby><rb>売</rb><rt>う</rt></ruby>る</span>さ」</p>
+		<p>「売ればいくらぐらいになって」</p>
+		<p>先生はいくらともいわなかった。けれども先生の話は、<span><ruby><rb>容易</rb><rt>ようい</rt></ruby>に</span><span><ruby><rb>自分</rb><rt>じぶん</rt></ruby>の</span>死という<span><ruby><rb>遠</rb><rt>とお</rt></ruby>い</span>問題を<span><ruby><rb>離</rb><rt>はな</rt></ruby>れなかった</span>。そうしてその死は<span><ruby><rb>必</rb><rt>かなら</rt></ruby>ず</span>奥さんの<span><ruby><rb>前</rb><rt>まえ</rt></ruby></span>に<span><ruby><rb>起</rb><rt>おこ</rt></ruby>る</span>ものと<span><ruby><rb>仮定</rb><rt>かてい</rt></ruby>されていた</span>。奥さんも<span><ruby><rb>最初</rb><rt>さいしょ</rt></ruby>の</span>うちは、わざと<span>たわいのない</span><span><ruby><rb>受</rb><rt>う</rt></ruby>け<ruby><rb>答</rb><rt>ごた</rt></ruby>え</span>をしているらしく<span><ruby><rb>見</rb><rt>み</rt></ruby>えた</span>。それが<span>いつの<ruby><rb>間</rb><rt>ま</rt></ruby>にか</span>、<span><ruby><rb>感傷的</rb><rt>かんしょうてき</rt></ruby>な</span><span><ruby><rb>女</rb><rt>おんな</rt></ruby>の</span><span><ruby><rb>心</rb><rt>こころ</rt></ruby></span>を<span><ruby><rb>重苦</rb><rt>おもぐる</rt></ruby>しくした</span>。</p>
+
+		<h3 class="section">Rough translation</h3>
+		<p>35</p>
+		<p>I'd been preparing to take my leave, but instead I made my self comfortable again. I didn't want to disrupt the conversation.</p>
+		<p>"What do you think?" Sensei asked me.</p>
+		<p>I had never, of course, given any thought to who would die first, Sensei or his wife. All I could do was laugh. "Don't look to me to tell you the number of your days."</p>
+		<p>"It really is about numbered days, isn't it? Each of us is born with a given amount, no more and no less. Sensei's mother and father, for example, departed this world nearly together."</p>
+		<p>"They both died on the same day?"</p>
+		<p>"Not the same day, but close to it. One after the other."</p>
+		<p>This was news to me. I was intrigued.</p>
+		<p>"How did they manage that?"</p>
+		<p>Sensei interrupted his wife before she could answer. "Let's not get into that. It's of no relevance."</p>
+		<p>Sensei held a fan in his hand, and he made a point of stirring the air with it. Then he turned back to his wife, "Shizu, after I'm gone this house will be yours."</p>
+		<p>She smiled. "How about the land it's on too?"</p>
+		<p>"I'm afraid the land's not mine to give, but all my possessions will be yours."</p>
+		<p>"That's fine, but what do I do with all those Western books?"</p>
+		<p>"Sell them to a dealer."</p>
+		<p>"How much are they worth?"</p>
+		<p>Sensei didn't answer her question. However, he remained fixated on the distant subject of his own death. He seemed convinced that his own death would precede his wife's. His wife, initially, made her best effort to take the discussion in stride. As it went on, though, the subject began to weigh on her feminine sensibilities.</p>
+
+		<h3 class="section">Vocabulary</h3>
+		<span class="vocabstudy"><ruby><rb>三十五</rb><rt>さんじゅうご</rt></ruby>&nbsp;(part) 35</span>
+		<span class="vocabstudy"><ruby><rb>私</rb><rt>わたくし</rt></ruby>&nbsp;I; me</span><span class="vocabstudy"><ruby><rb>立</rb><rt>た</rt></ruby>て<ruby><rb>掛</rb><rt>か</rt></ruby>けた&nbsp;(had) started to raise</span><span class="vocabstudy"><ruby><rb>腰</rb><rt>こし</rt></ruby>&nbsp;hips; haunches</span><span class="vocabstudy"><ruby><rb>話</rb><rt>はなし</rt></ruby>&nbsp;talk; conversation</span><span class="vocabstudy"><ruby><rb>区切</rb><rt>くぎ</rt></ruby>りの<ruby><rb>付</rb><rt>つ</rt></ruby>く&nbsp;come to a pause; come to a break point</span><span class="vocabstudy"><ruby><rb>二人</rb><rt>ふたり</rt></ruby>&nbsp;two (people)</span><span class="vocabstudy"><ruby><rb>相手</rb><rt>あいて</rt></ruby>&nbsp;companion; company</span>
+		<span class="vocabstudy"><ruby><rb>君</rb><rt>きみ</rt></ruby>&nbsp;you</span><span class="vocabstudy"><ruby><rb>思</rb><rt>おも</rt></ruby>います&nbsp;think</span><span class="vocabstudy"><ruby><rb>先生</rb><rt>せんせい</rt></ruby>&nbsp;Sensei (elder one; teacher - used here as form of address)</span><span class="vocabstudy"><ruby><rb>聞</rb><rt>き</rt></ruby>いた&nbsp;asked</span>
+		<span class="vocabstudy"><ruby><rb>先</rb><rt>さき</rt></ruby>&nbsp;before; ahead</span><span class="vocabstudy"><ruby><rb>死</rb><rt>し</rt></ruby>ぬ&nbsp;die; depart</span><span class="vocabstudy"><ruby><rb>奥</rb><rt>おく</rt></ruby>さん&nbsp;(Sensei's) wife</span><span class="vocabstudy"><ruby><rb>早</rb><rt>はや</rt></ruby>く&nbsp;earlier; sooner</span><span class="vocabstudy"><ruby><rb>亡</rb><rt>な</rt></ruby>くなる&nbsp;pass away</span><span class="vocabstudy"><ruby><rb>固</rb><rt>もと</rt></ruby>より&nbsp;from the start; by nature</span><span class="vocabstudy"><ruby><rb>判断</rb><rt>はんだん</rt></ruby>&nbsp;judgment; opinion</span><span class="vocabstudy"><ruby><rb>問題</rb><rt>もんだい</rt></ruby>&nbsp;problem; question</span><span class="vocabstudy"><ruby><rb>笑</rb><rt>わら</rt></ruby>っていた&nbsp;laughed</span>
+		<span class="vocabstudy"><ruby><rb>寿命</rb><rt>じゅみょう</rt></ruby>&nbsp;(one's) life span</span><span class="vocabstudy"><ruby><rb>分</rb><rt>わか</rt></ruby>りません&nbsp;can't know</span>
+		<span class="vocabstudy"><ruby><rb>本当</rb><rt>ほんとう</rt></ruby>に&nbsp;really; truly</span><span class="vocabstudy"><ruby><rb>生</rb><rt>うま</rt></ruby>れた&nbsp;be born</span><span class="vocabstudy"><ruby><rb>時</rb><rt>とき</rt></ruby>&nbsp;time</span><span class="vocabstudy"><ruby><rb>極</rb><rt>きま</rt></ruby>った&nbsp;determined; certain</span><span class="vocabstudy"><ruby><rb>年数</rb><rt>ねんすう</rt></ruby>&nbsp;number of years</span><span class="vocabstudy">もらって<ruby><rb>来</rb><rt>く</rt></ruby>る&nbsp;come with; bring with</span><span class="vocabstudy"><ruby><rb>仕方</rb><rt>しかた</rt></ruby>がない&nbsp;is inevitable; is fate</span><span class="vocabstudy">お<ruby><rb>父</rb><rt>とう</rt></ruby>さん&nbsp;father</span><span class="vocabstudy">お<ruby><rb>母</rb><rt>かあ</rt></ruby>さん&nbsp;mother</span><span class="vocabstudy"><ruby><rb>同</rb><rt>おんな</rt></ruby>じ&nbsp;the same</span>
+		<span class="vocabstudy"><ruby><rb>日</rb><rt>ひ</rt></ruby>&nbsp;day</span>
+		<span class="vocabstudy"><ruby><rb>続</rb><rt>つづ</rt></ruby>いて&nbsp;in succession; one after another</span>
+		<span class="vocabstudy"><ruby><rb>知識</rb><rt>ちしき</rt></ruby>&nbsp;knowledge; information</span><span class="vocabstudy"><ruby><rb>新</rb><rt>あたら</rt></ruby>しい&nbsp;new</span><span class="vocabstudy"><ruby><rb>不思議</rb><rt>ふしぎ</rt></ruby>&nbsp;strange; curious</span>
+		<span class="vocabstudy"><ruby><rb>一度</rb><rt>いちど</rt></ruby>に&nbsp;at once; at the same time</span>
+		<span class="vocabstudy"><ruby><rb>問</rb><rt>と</rt></ruby>い&nbsp;question; inquiry</span><span class="vocabstudy"><ruby><rb>答</rb><rt>こた</rt></ruby>えよう&nbsp;answer; respond</span><span class="vocabstudy"><ruby><rb>遮</rb><rt>さえぎ</rt></ruby>った&nbsp;interrupted</span>
+		<span class="vocabstudy">お<ruby><rb>止</rb><rt>よ</rt></ruby>し&nbsp;stop; dispense with</span>
+		<span class="vocabstudy"><ruby><rb>手</rb><rt>て</rt></ruby>&nbsp;hand</span><span class="vocabstudy"><ruby><rb>持</rb><rt>も</rt></ruby>った&nbsp;held</span><span class="vocabstudy"><ruby><rb>団扇</rb><rt>うちわ</rt></ruby>&nbsp;round (fan)</span><span class="vocabstudy"><ruby><rb>顧</rb><rt>かえり</rt></ruby>みた&nbsp;turned back to</span>
+		<span class="vocabstudy"><ruby><rb>静</rb><rt>しず</rt></ruby>&nbsp;Shizu (name of Sensei's wife)</span><span class="vocabstudy"><ruby><rb>家</rb><rt>うち</rt></ruby>&nbsp;house</span><span class="vocabstudy">お<ruby><rb>前</rb><rt>まえ</rt></ruby>&nbsp;you</span>
+		<span class="vocabstudy"><ruby><rb>笑</rb><rt>わら</rt></ruby>い<ruby><rb>出</rb><rt>だ</rt></ruby>した&nbsp;laughed</span>
+		<span class="vocabstudy"><ruby><rb>地面</rb><rt>じめん</rt></ruby>&nbsp;land; property</span><span class="vocabstudy"><ruby><rb>下</rb><rt>くだ</rt></ruby>さい&nbsp;please (give me)</span>
+		<span class="vocabstudy"><ruby><rb>他</rb><rt>ひと</rt></ruby>&nbsp;other person; another person</span><span class="vocabstudy">その<ruby><rb>代</rb><rt>かわ</rt></ruby>り&nbsp;instead (of); in exchange (for)</span><span class="vocabstudy"><ruby><rb>皆</rb><rt>みん</rt></ruby>な&nbsp;all; everything</span>
+		<span class="vocabstudy"><ruby><rb>有難</rb><rt>ありがと</rt></ruby>う&nbsp;(I) appreciate it</span><span class="vocabstudy"><ruby><rb>横文字</rb><rt>よこもじ</rt></ruby>&nbsp;Western language; Roman letters</span><span class="vocabstudy"><ruby><rb>本</rb><rt>ほん</rt></ruby>&nbsp;books; texts</span><span class="vocabstudy"><ruby><rb>貰</rb><rt>もら</rt></ruby>って&nbsp;receive</span>
+		<span class="vocabstudy"><ruby><rb>古本屋</rb><rt>ふるほんや</rt></ruby>&nbsp;used book store; used book dealer</span><span class="vocabstudy"><ruby><rb>売</rb><rt>う</rt></ruby>る&nbsp;sell</span>
+		<span class="vocabstudy"><ruby><rb>容易</rb><rt>ようい</rt></ruby>に&nbsp;easily; readily</span><span class="vocabstudy"><ruby><rb>自分</rb><rt>じぶん</rt></ruby>の&nbsp;one's own</span><span class="vocabstudy"><ruby><rb>遠</rb><rt>とお</rt></ruby>い&nbsp;distant; far off</span><span class="vocabstudy"><ruby><rb>離</rb><rt>はな</rt></ruby>れなかった&nbsp;did not leave; did not move away from</span><span class="vocabstudy"><ruby><rb>必</rb><rt>かなら</rt></ruby>ず&nbsp;invariably</span><span class="vocabstudy"><ruby><rb>前</rb><rt>まえ</rt></ruby>&nbsp;before</span><span class="vocabstudy"><ruby><rb>起</rb><rt>おこ</rt></ruby>る&nbsp;happen; take place</span><span class="vocabstudy"><ruby><rb>仮定</rb><rt>かてい</rt></ruby>されていた&nbsp;was imagined; was presumed (to be)</span><span class="vocabstudy"><ruby><rb>最初</rb><rt>さいしょ</rt></ruby>の&nbsp;first; initial</span><span class="vocabstudy">たわいのない&nbsp;playful; guileless</span><span class="vocabstudy"><ruby><rb>受</rb><rt>う</rt></ruby>け<ruby><rb>答</rb><rt>ごた</rt></ruby>え&nbsp;replies; responses</span><span class="vocabstudy"><ruby><rb>見</rb><rt>み</rt></ruby>えた&nbsp;appeared to ...</span><span class="vocabstudy">いつの<ruby><rb>間</rb><rt>ま</rt></ruby>にか&nbsp;in time; before one knew it</span><span class="vocabstudy"><ruby><rb>感傷的</rb><rt>かんしょうてき</rt></ruby>な&nbsp;sentimental; emotional</span><span class="vocabstudy"><ruby><rb>女</rb><rt>おんな</rt></ruby>の&nbsp;female; woman's</span><span class="vocabstudy"><ruby><rb>心</rb><rt>こころ</rt></ruby>&nbsp;heart; mind; soul</span><span class="vocabstudy"><ruby><rb>重苦</rb><rt>おもぐる</rt></ruby>しくした&nbsp;weighed down; oppressed</span>
+
+	</StudyGuide>
+);
