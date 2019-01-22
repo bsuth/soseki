d821d4eb69d92d451cfe9ffac06d9903df7cb84b
bsuth <bsuth@umich.edu>
2019-01-21 20:15:06 -0500

    created nodes for botchan, sanshiro, and kokoro indices

---
+++ src/pages/kokoro.js
@@ -0,0 +1,99 @@
+// React
+import React from 'react'
+
+// Components
+import ChapterSelect from 'components/ChapterSelect'
+
+// Images
+import Cover from 'images/kokoroindex.jpg'
+
+
+export default () => (
+	<ChapterSelect
+		root='/kokoro'
+		imgSrc={Cover}
+		imgAlt="Soseki in his Study"
+		Left={ChaptersLeft}
+		Right={ChaptersRight}
+		colorPrimary='#dedede'
+		colorSecondary='#2d2d2d'
+	/>
+);
+
+const ChaptersLeft = [
+	{
+		title: 'Sensei and I 01-09',
+		desc: 'seaside ... domestic discord',
+		sections: '001 - 018',
+		path: '/kokoro/chapter1',
+	},
+	{
+		title: 'Sensei and I 10-18',
+		desc: 'idle life ... holdering the fort',
+		sections: '019 - 036',
+		path: '/kokoro/chapter2',
+	},
+	{
+		title: 'Sensei and I 19-27',
+		desc: 'father\'s illness ... thesis',
+		sections: '037 - 054',
+		path: '/kokoro/chapter3',
+	},
+	{
+		title: 'Sensei and I 28-36',
+		desc: 'family assets .. graduation',
+		sections: '055 - 072',
+		path: '/kokoro/chapter4',
+	},
+	{
+		title: 'My Parents and I 01-09',
+		desc: 'back home ... father\'s relapse',
+		sections: '073 - 090',
+		path: '/kokoro/chapter5',
+	},
+	{
+		title: 'My Parents and I 10-18',
+		desc: 'caregiving',
+		sections: '091 - 108',
+		path: '/kokoro/chapter6',
+	},
+];
+
+const ChaptersRight = [
+	{
+		title: 'Sensei\'s Testament 01-09',
+		desc: 'deceit ... cashing out',
+		sections: '109 - 126',
+		path: '/kokoro/chapter7',
+	},
+	{
+		title: 'Sensei\'s Testament 10-18',
+		desc: 'quieter lodgings',
+		sections: '127 - 144',
+		path: '/kokoro/chapter8',
+	},
+	{
+		title: 'Sensei\'s Testament 19-27',
+		desc: 'bringing in K',
+		sections: '145 - 162',
+		path: '/kokoro/chapter9',
+	},
+	{
+		title: 'Sensei\'s Testament 28-36',
+		desc: 'Bōshū ... K\'s confession',
+		sections: '163 - 180',
+		path: '/kokoro/chapter10',
+	},
+	{
+		title: 'Sensei\'s Testament 37-46',
+		desc: 'rivalry ... betrothal',
+		sections: '181 - 200',
+		path: '/kokoro/chapter11',
+	},
+	{
+		title: 'Sensei\'s Testament 47-56',
+		desc: 'endings',
+		sections: '201 - 220',
+		path: '/kokoro/chapter12',
+	},
+];
---
+++ src/pages/sanshiro.js
@@ -0,0 +1,104 @@
+// React
+import React from 'react'
+
+// Components
+import ChapterSelect from 'components/ChapterSelect'
+
+// Images
+import Cover from 'images/sanshiroindex.jpg'
+
+
+export default () => (
+	<ChapterSelect
+		imgSrc={Cover}
+		imgAlt="Sanshiro Index Image"
+		Left={ChaptersLeft}
+		Right={ChaptersRight}
+		colorPrimary='#f7e8f7'
+		colorSecondary='#610561'
+	/>
+);
+
+const ChaptersLeft = [
+	{
+		title: 'Chapter 1',
+		desc: 'traveling to Tōkyō',
+		sections: '001 - 015',
+		path: '/sanshiro/chapter1',
+	},
+	{
+		title: 'Chapter 2',
+		desc: 'meeting Nonomiya',
+		sections: '016 - 026',
+		path: '/sanshiro/chapter2',
+	},
+	{
+		title: 'Chapter 3',
+		desc: 'lectures ... hospital girls',
+		sections: '027 - 053',
+		path: '/sanshiro/chapter3',
+	},
+	{
+		title: 'Chapter 4',
+		desc: 'moving Professor Hirota',
+		sections: '054 - 084',
+		path: '/sanshiro/chapter4',
+	},
+	{
+		title: 'Chapter 5',
+		desc: 'stray sheep',
+		sections: '085 - 103',
+		path: '/sanshiro/chapter5',
+	},
+	{
+		title: 'Chapter 6',
+		desc: 'great dark void ... field day',
+		sections: '104 - 130',
+		path: '/sanshiro/chapter6',
+	},
+	{
+		title: 'Chapter 7',
+		desc: 'Hirota and Haraguchi',
+		sections: '131 - 144',
+		path: '/sanshiro/chapter7',
+	},
+];
+
+const ChaptersRight = [
+	{
+		title: 'Chapter 8',
+		desc: 'calling on Mineko',
+		sections: '145 - 166',
+		path: '/sanshiro/chapter8',
+	},
+	{
+		title: 'Chapter 9',
+		desc: 'party ... money from home',
+		sections: '167 -186',
+		path: '/sanshiro/chapter9',
+	},
+	{
+		title: 'Chapter 10',
+		desc: 'Haraguchi painting Mineko',
+		sections: '187 - 205',
+		path: '/sanshiro/chapter10',
+	},
+	{
+		title: 'Chapter 11',
+		desc: 'Reiyoshi exposed',
+		sections: '206 - 221',
+		path: '/sanshiro/chapter11',
+	},
+	{
+		title: 'Chapter 12',
+		desc: 'at the show ... church',
+		sections: '222 - 239',
+		path: '/sanshiro/chapter12',
+	},
+	{
+		title: 'Chapter 13',
+		desc: 'Mineko\'s painting',
+		sections: '240 - 241',
+		path: '/sanshiro/chapter13',
+	},
+];
