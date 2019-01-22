456e92b0738ea416eb1643627a033a1ce74bffe9
bsuth <bsuth@umich.edu>
2019-01-22 18:11:38 -0500

    edit section break for case when audio doesn't exist

--- src/components/SectionBreak.js
+++ src/components/SectionBreak.js
@@ -13,19 +13,32 @@ import styles from './SectionBreak.module.scss'
 // This component renders the section breaks 
 // in the chapter texts.
 
-export default ({studyguide, num, audio}) => (
-	<div className={styles.section_break}>
-		<h3> section {num.toString().padStart(3, '0')} </h3>
+export default ({studyguide, num, audio}) => {
 
-		<Link to={studyguide}>
-			study guide
-		</Link>
+	// Check to make sure the audio exists.
+	// (this is a problem for the last sections
+	// of The Tower of London).
+	let audioComponent;
+	if(!audio) {
+		audioComponent = <span>audio not available for this section</span>;
+	} else {
+		audioComponent = (
+			<audio controls="controls" preload="none">
+				<source src={audio}/>
+				This browser does not support HTML5 audio
+			</audio>
+		);
+	}
 
-		<audio controls="controls" preload="none">
-			<source src={audio}/>
-			This browser does not support HTML5 audio
-		</audio>
-	</div>
-);
+	return(
+		<div className={styles.section_break}>
+			<h3> section {num.toString().padStart(3, '0')} </h3>
+			<Link to={studyguide}>
+				study guide
+			</Link>
+			{ audioComponent }
+		</div>
+	);
+};
 
 ////////// SECTION BREAK //////////
