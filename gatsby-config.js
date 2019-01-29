4f5ad8da9ccb662f17eb5d5528b73a604b4753f1
bsuth <bsuth@umich.edu>
2019-01-29 18:24:11 -0500

    added plugins to make app more PWA compliant

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	plugins: [
+		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-sass',
 		'gatsby-mdx',
 		{
@@ -15,5 +16,21 @@ module.exports = {
         path: `${__dirname}/src/text/`,
       },
     },
+		{
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Soseki Project",
+        short_name: "Soseki",
+        start_url: "/",
+        background_color: "#181818",
+        theme_color: "#e8e8e8",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone,
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+      },
+    },
+		// This must be placed after the manifest plugin
+		'gatsby-plugin-offline',
 	]
 }
